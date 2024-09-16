4381a4de1703fadfcba63096606e2d6b2cd1317c
xdkomel <xdghost.kamil@gmail.com>
2024-09-16 18:32:44 +0300

    try defining the authToken in the build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
+allprojects {
+    repositories {
+        google()
+        maven {
+            setUrl("https://jitpack.io")
+            credentials {
+                username = authToken
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,30 +1,20 @@
 pluginManagement {
-    val properties = java.util.Properties().apply {
-        file("gradle.properties").inputStream().use(::load)
-    }
-    val authToken: String? = properties.getProperty("authToken")
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
         maven { 
             url = uri("https://jitpack.io")
-            credentials { username = authToken ?: "" }
         }
     }
 }
 dependencyResolutionManagement {
-    val properties = java.util.Properties().apply {
-        file("gradle.properties").inputStream().use(::load)
-    }
-    val authToken: String? = properties.getProperty("authToken")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         maven { 
             url = uri("https://jitpack.io")
-            credentials { username = authToken ?: "" }
         }
         maven { 
             url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") 
