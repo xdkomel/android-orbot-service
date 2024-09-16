d52068856b4ab956a7aa9ca34b824fb0ce8e7463
xdkomel <xdghost.kamil@gmail.com>
2024-09-16 17:40:05 +0300

    try reading property in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,30 @@
 pluginManagement {
+    val properties = java.util.Properties().apply {
+        file("gradle.properties").inputStream().use(::load)
+    }
+    val authToken: String? = properties.getProperty("authToken")
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
         maven { 
             url = uri("https://jitpack.io")
-            credentials { username = System.getProperty("authToken") }
+            credentials { username = authToken ?: "" }
         }
     }
 }
 dependencyResolutionManagement {
+    val properties = java.util.Properties().apply {
+        file("gradle.properties").inputStream().use(::load)
+    }
+    val authToken: String? = properties.getProperty("authToken")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         maven { 
             url = uri("https://jitpack.io")
-            credentials { username = System.getProperty("authToken") }
+            credentials { username = authToken ?: "" }
         }
         maven { 
             url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") 
